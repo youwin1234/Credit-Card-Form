bc2fa77503166ec6cecf5f1797d8865445e2512a
youwin1234 <tebbytoad@gmail.com>
2020-05-05 11:28:18 -0400

    Integrated working masking of CC into form.
    need to alter regex of CC format to get the masking included into the format to each individual CC type.

--- src/app/card-number-form-control.ts
+++ src/app/card-number-form-control.ts
@@ -6,12 +6,20 @@ export class CardNumberFormControl extends FormControl{
             super.setValue('', {...options, emitModelToViewChange:true});
             return;
         }
-        if(value.match(/[^0-9|\/]/gi)){
+        if(value.match(/[^0-9|\-]/gi)){
             super.setValue(this.value, {...options, emitModelToViewChange: true});
             return;
         }
-        if(value.length>16){
+        if(value.length>19){
             super.setValue(this.value, {...options, emitModelToViewChange:true});
+            return;
+        }
+        if((value.length === 4 && this.value.length === 5) || (value.length === 9 && this.value.length === 10) || (value.length === 14 && this.value.length === 15)){
+            super.setValue(value, {...options, emitModelToViewChange: true});
+            return;
+        }
+        if(value.length === 4 || value.length === 9 || value.length === 14){
+            super.setValue(value + '-', {...options, emitModelToViewChange: true});
             return;
         }
         super.setValue(value, {...options, emitModelToViewChange: true});
